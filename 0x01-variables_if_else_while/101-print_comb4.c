ca947d179ce17eed63e86adec31aefecd618dfde
Daltone123 <daltoneotieno123@gmail.com>
2023-08-15 00:38:25 +0300

    a program that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,19 +12,21 @@ int main(void)
 		j = i / 100;
 		k = (i / 10) % 10;
 		l = i % 10;
+
+		if (j < k && k < l)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+			if (i < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
 	}
-	if (j < k && k < l)
-	{
-		putchar(j + '0');
-		putchar(k + '0');
-		putchar(l + '0');
-	}
-	if (i < 700)
-	{
-		putchar(44);
-		putchar(32);
-	}
-																					}
-putchar('\n');
-return (0);
+	putchar('\n');
+
+	return (0);
+
 }
